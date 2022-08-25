610b27939747737db590a1d7ceaed9a614ee44b2
rokoner79 <79681506+rokoner79@users.noreply.github.com>
2022-08-25 21:31:22 +0600

    Add files via upload

---
+++ multiply.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter value of a\n");
+    scanf("%d",&a);
+    printf("Enter value of b\n");
+    scanf("%d",&b);
+    printf("Enter value of c\n");
+    scanf("%d",&c);
+    double mul=a*b*c;
+    printf("Summation is: %f",mul);
+    return 0;
+}
+
---
+++ summationuserdefine.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter value of a\n");
+    scanf("%d",&a);
+    printf("Enter value of b\n");
+    scanf("%d",&b);
+    printf("Enter value of c\n");
+    scanf("%d",&c);
+    int sum=a+b+c;
+    printf("Summation is: %d",sum);
+    return 0;
+}
